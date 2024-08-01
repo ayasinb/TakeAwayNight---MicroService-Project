2f02c828ae3c40051b87cefb159bbfd2be177f1e
Yasin Baltaoğlu <37988334+ayasinb@users.noreply.github.com>
2024-08-02 01:25:36 +0300

    yeni microservisler eklendi. web ui eklendi. geliştiriliyor.

---
+++ Frontends/TakeAwayNight.WebUI/Dtos/CatalogDtos/ResultProductDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TakeAwayNight.WebUI.Dtos.CatalogDtos
+{
+    public class ResultProductDto
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ImageURL { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ Services/Basket/TakeAwayNight.Basket/Settings/RedisService.cs
@@ -0,0 +1,23 @@
+﻿using StackExchange.Redis;
+
+namespace TakeAwayNight.Basket.Settings
+{
+    public class RedisService
+    {
+        public string _Host { get; set; }
+        public int _Port { get; set; }
+        public RedisService(string host, int port)
+        {
+            _Host = host;
+            _Port = port;
+        }
+
+       
+
+        public ConnectionMultiplexer _connectionMultiplexer;
+
+        public void Connect() => _connectionMultiplexer = ConnectionMultiplexer.Connect($"{_Host}:{_Port}");
+
+        public IDatabase GetDb(int db = 1) => _connectionMultiplexer.GetDatabase(0);
+    }
+}
