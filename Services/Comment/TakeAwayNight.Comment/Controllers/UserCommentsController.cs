fc1dbf4b4b1b0e442f99f6cbb6948e711f2cce85
Yasin Baltaoğlu <37988334+ayasinb@users.noreply.github.com>
2024-08-06 21:57:25 +0300

    Comment mikroservisi tamamlandı.

---
+++ Services/Comment/TakeAwayNight.Comment/Controllers/UserCommentsController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.ValueGeneration.Internal;
+using TakeAwayNight.Comment.Dtos;
+using TakeAwayNight.Comment.Services;
+
+namespace TakeAwayNight.Comment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserCommentsController : ControllerBase
+    {
+        private readonly ICommentService _commentService;
+
+        public UserCommentsController(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllUserComment()
+        { 
+            var values=await _commentService.GetAllUserCommentAsync();
+            return Ok(values);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateUserComment(CreateUserCommentDto createUserCommentDto)
+        {
+            await _commentService.CreateUserCommentAsync(createUserCommentDto);
+            return Ok("Yorum Eklendi.");
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteUserComment(int id)
+        {
+            await _commentService.DeleteUserCommentAsync(id);
+            return Ok("Yorum Silindi.");
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateUserComment(UpdateUserCommentDto updateUserCommentDto)
+        {
+            await _commentService.UpdateUserCommentAsync(updateUserCommentDto);
+            return Ok("Yorum Güncellendi");
+        }
+        [HttpGet("UserCommentByProductId")]
+        public async Task<IActionResult> UserCommentByProductId(string id)
+        {
+            var values=await _commentService.GetByProductIdUserCommentAsync(id);
+            return Ok(values);
+        }
+        [HttpGet("GetUserComment")]
+        public async Task<IActionResult> GetUserComment(int id)
+        {
+            var values= await _commentService.GetByIdUserCommentAsync(id);
+            return Ok(values);
+        }
+
+    }
+}
--- Services/Comment/TakeAwayNight.Comment/Program.cs
+++ Services/Comment/TakeAwayNight.Comment/Program.cs
@@ -1,6 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using TakeAwayNight.Comment.DAL.Context;
+using TakeAwayNight.Comment.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<CommentContext>(x =>
+{
+    x.UseNpgsql(connectionString);
+});
+builder.Services.AddScoped<ICommentService, CommentService>();
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
